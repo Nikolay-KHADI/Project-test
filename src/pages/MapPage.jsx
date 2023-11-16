cc3b165b3df8b986e1f1db93ae47c91be48a7bf4
Nikolay-KMS <karpuk.nik@gmail.com>
2023-11-16 16:38:18 +0200

    test 1

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -3,14 +3,14 @@
 import { useLoadScript } from "@react-google-maps/api"
 import { Map } from '../components/Map';
 
-const libraries = ["places"]
+// const libraries = ["places"]
 
 
 export function MapPage() {
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_MAP_API_KEY,
-    libraries: libraries,
+    // libraries: libraries,
   });
 
   if (!isLoaded) return <div>Loading...</div>
